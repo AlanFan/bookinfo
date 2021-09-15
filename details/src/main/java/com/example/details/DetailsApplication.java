8e108e8ff6a2e533a0970531f0d73e83ba35f424
fanpeilun <fanpeilun@163.com>
2021-09-15 18:38:10 +0800

    env

--- details/src/main/java/com/example/details/DetailsApplication.java
+++ details/src/main/java/com/example/details/DetailsApplication.java
@@ -1,5 +1,6 @@
 package com.example.details;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DetailsApplication {
 
+	@Value("${version: not-set}")
+	String version;
+
 	@GetMapping("/name")
 	public String name(){
 
-		return "world (v1)";
+		return "world " + version;
 	}
 
 	public static void main(String[] args) {
